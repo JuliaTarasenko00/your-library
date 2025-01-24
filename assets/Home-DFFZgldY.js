ebc95e6be522e495c439f6ea7f2ee5276a9cb3ba
JuliaTarasenko00 <118853187+JuliaTarasenko00@users.noreply.github.com>
2025-01-24 10:16:46 +0000

    Deploying to gh-pages from @ JuliaTarasenko00/your-library@86ce62e9fa65505cca4857eb975988537381bd58 🚀

---
+++ assets/Home-DFFZgldY.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Dmf8GAkc.js";function o(){return e.jsx("section",{children:e.jsx("p",{children:"Recommended"})})}export{o as default};
--- assets/Home-qerpnKm4.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BupoXkMg.js";function o(){return e.jsx("section",{className:"container",children:e.jsx("p",{children:"Home"})})}export{o as default};
